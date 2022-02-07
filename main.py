c9659b710aeb8d90dabfc69f71a2e0f959599ef5
Judd Baroff <self@juddbaroff.com>
2022-02-07 16:14:02 -0600

    allow user to pick category and difficulty

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from question_model import Question
-from data import question_data
+from data import question_cooker, set_category, set_difficulty
 from quiz_brain import QuizBrain
 from logo import logo
 
 print(logo)
 
+input("Please press any button to list your category questions:\n")
+question_dict = question_cooker(cat=set_category(), difficulty=set_difficulty())
+
 question_bank = []
-for n in range(0, len(question_data)):
-    text = question_data[n]["question"]
-    answer = question_data[n]["correct_answer"]
+for n in range(0, len(question_dict)):
+    text = question_dict[n]["question"]
+    answer = question_dict[n]["correct_answer"]
     question_object = Question(text, answer)
     question_bank.append(question_object)
 
